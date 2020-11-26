b2051254b2527e6d080f9e1827917f2c25b2126b
AIDOUD Trad <trad.aidoud@gmail.com>
2020-11-26 06:00:49 +0100

    redux with react switcher app

---
+++ src/lightComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+const mapStateToProps = (state) => ({
+  light: state,
+});
+
+const LightComponent = ({ light, dispatch }) => (
+  <div>
+    <div>{light}</div>
+    <button onClick={() => dispatch({ type: "SWITCH" })}>switch</button>
+  </div>
+);
+
+export default connect(mapStateToProps)(LightComponent);
